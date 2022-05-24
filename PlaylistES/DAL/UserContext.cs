01bdac04b17a94b10ea09f6c8ebdb8943987f042
TomasOrnelas <tomaspxo98@gmail.com>
2022-05-24 17:00:03 +0100

    🐒

---
+++ PlaylistES/DAL/UserContext.cs
@@ -0,0 +1,21 @@
+﻿using PlaylistES.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+namespace PlaylistES.DAL
+{
+  
+    public class UserContext : DbContext
+    {
+
+        public UserContext() : base("UserContext")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+   
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ PlaylistES/DAL/UserInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using PlaylistES.Models;
+namespace PlaylistES.DAL
+{
+    public class UserInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<UserContext>
+    {
+        protected override void Seed(UserContext context)
+        {
+            var users = new List<User>
+            {
+            new User{username="Hello",password="123456",Profilepicture="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMQsB3l79Js4_u-Z4kQ8PyyYCo_8QOM8bTdw&usqp=CAU"},
+            new User{username="Greetings",password="123456",Profilepicture="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMQsB3l79Js4_u-Z4kQ8PyyYCo_8QOM8bTdw&usqp=CAU"},         
+            };
+
+            users.ForEach(s => context.Users.Add(s));
+            context.SaveChanges();
+           
+        }
+    }
+}
